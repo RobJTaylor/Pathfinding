b2b7f8924aafa817a73c8c7bfcbe4a03012dd3da
Rob Taylor <robert.j.taylor117@gmail.com>
2019-03-26 23:56:54 +0000

    Commented GridSpace

--- Pathfinding/GridSpace.cs
+++ Pathfinding/GridSpace.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Pathfinding
 {
+    /**
+     * GridSpace - Placed on the grid. Contains info about space type and unit occupying it.
+     */
     class GridSpace
     {
         private int spaceType;
@@ -14,24 +17,40 @@ namespace Pathfinding
         public GridSpace(int spaceType)
         {
             this.spaceType = spaceType;
+            //Set to -1 by default, a unit will change this if necessary
             this.occupant = -1;
         }
 
+        /**
+         * GetSpaceType - get the space type
+         * Returns int - 0 = moveable terrain, 1 = immoveable terrain
+         */
         public int GetSpaceType()
         {
             return this.spaceType;
         }
         
+        /**
+         * GetSpaceOccupant - gets a space occupant, if they exist
+         * Returns int - -1 = no unit | greater than 0 = unit
+         */
         public int GetSpaceOccupant()
         {
             return this.occupant;
         }
 
+        /**
+         * SetSpaceOccupant - set the space occupant
+         * int occupantId - the unit id occupying the space
+         */
         public void SetSpaceOccupant(int occupantId)
         {
             this.occupant = occupantId;
         }
 
+        /**
+         * RemoveOccupant - remove the occupant from the space
+         */
         public void RemoveOccupant()
         {
             this.occupant = -1;
